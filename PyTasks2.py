4b38b2d09be53107f8178b0174b12581b9955bd2
ProstoNikita <nikita.iryupin@email.cz>
2019-08-20 14:09:03 +0300

    Exercises

--- PyTasks2.py
+++ PyTasks2.py
@@ -1,12 +1,31 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-a = st1 = input()
-i, a = 0, 1
-while i != (len(st1) - 1):
-    if st1[i] == st1[i+1]:
-        a += 1
-    if st1[i] != st1[i+1]:
-        st2 = st1[i] + str(a)
-    i += 1
-print(st2)
+#st1 = input() + "."
+#i, a = 0, 1
+#st2 = ""
+#while i != (len(st1) - 1):
+#    if st1[i] == st1[i+1]:
+#        a += 1
+#    else:
+#        st2 += st1[i] + str(a)
+#        a = 1
+#    i += 1
+#print(st2)
+
+#for student in students:
+#    print ("Hello, " + student + "!")
+
+#students = ['Ivan', 'Masha', 'Sasha']
+#students += ['Olga']
+#students += 'Olga'
+#print((students))
+
+#students = ['Ivan', 'Masha', 'Sasha']
+#students.remove('Sasha')
+#del students[0]
+#print(students)
+
+students = ['Sasha', 'Ivan', 'Masha']
+students = sorted(students)
+print(students)
